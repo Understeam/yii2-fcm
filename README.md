ab76ae5c39d6ebf8a82c6d9cfd2de914f93606ff
arugalev <rugalev@enaza.ru>
2016-11-11 18:22:11 +0500

    Improvements: * Added README * Simplified usage of `createMessage`

--- README.md
+++ README.md
@@ -1,3 +1,46 @@
 # Yii2 Firebase Cloud Messaging
 
+This component wraps [paragraph1/php-fcm](https://packagist.org/packages/paragraph1/php-fcm) library.
+
+## Installation
+
+Preferred way to install is through [Composer](https://getcomposer.org): 
+
+```shell
+$ composer require understeam/yii2-fcm:~0.1 --prefer-dist 
+```
+
+## Configuration
+
+Add component to your application config:
+
+```php
+...
+'components' => [
+    'fcm' => [
+        'class' => 'understeam\fcm\Client',
+        'apiKey' => 'your API key', // Server API Key (you can get it here: https://firebase.google.com/docs/server/setup#prerequisites) 
+    ],
+],
+...
+```
+
+## Usage
+
+You can find more usage examples [here](https://packagist.org/packages/paragraph1/php-fcm). 
+
+```php
+$note = Yii::$app->fcm->createNotification("test title", "testing body");
+$note->setIcon('notification_icon_resource_name')
+    ->setColor('#ffffff')
+    ->setBadge(1);
+
+$message = Yii::$app->fcm->createMessage();
+$message->addRecipient(new Device('your-device-token'));
+$message->setNotification($note)
+    ->setData(['someId' => 111]);
+
+$response = Yii::$app->fcm->send($message);
+var_dump($response->getStatusCode());
+```
 
--- src/Client.php
+++ src/Client.php
@@ -4,8 +4,10 @@ namespace understeam\fcm;
 
 use paragraph1\phpFCM\Message;
 use paragraph1\phpFCM\Notification;
+use paragraph1\phpFCM\Recipient\Device;
 use Yii;
 use yii\base\Component;
+use yii\base\InvalidParamException;
 
 /**
  * Client class dispatches paragraph1\phpFCM entities
@@ -35,8 +37,10 @@ class Client extends Component
     public $guzzleClass = '\GuzzleHttp\Client';
 
     private $_fcm;
+    private $_httpClient;
 
     /**
+     * Returns FCM client
      * @return \paragraph1\phpFCM\Client
      */
     public function getFcm()
@@ -48,6 +52,7 @@ class Client extends Component
     }
 
     /**
+     * Creates FCM client
      * @return \paragraph1\phpFCM\Client
      */
     protected function createFcm()
@@ -60,21 +65,56 @@ class Client extends Component
     }
 
     /**
+     * Returns Guzzle client
      * @return \GuzzleHttp\ClientInterface
      */
     public function getHttpClient()
     {
-        return Yii::createObject($this->guzzleClass, [$this->guzzleConfig]);
+        if (!isset($this->_httpClient)) {
+            $this->_httpClient = Yii::createObject($this->guzzleClass, [$this->guzzleConfig]);
+        }
+        return $this->_httpClient;
     }
 
+    /**
+     * Creates Notification object
+     * @param string $title Notification title
+     * @param string $body Notification body text
+     * @return Notification
+     */
     public function createNotification($title, $body)
     {
         return new Notification($title, $body);
     }
 
-    public function createMessage()
+    /**
+     * Creates Message object
+     * @param string[]|string $deviceTokens tokens of recipient devices
+     * @return Message
+     */
+    public function createMessage($deviceTokens = [])
     {
-        return new Message();
+        $message = new Message();
+        if (is_string($deviceTokens)) {
+            $deviceTokens = [$deviceTokens];
+        }
+        if (!is_array($deviceTokens)) {
+            throw new InvalidParamException("\$deviceTokens must be string or array");
+        }
+        foreach ($deviceTokens as $token) {
+            $message->addRecipient($this->createDevice($token));
+        }
+        return $message;
+    }
+
+    /**
+     * Creates Device object
+     * @param string $token
+     * @return Device
+     */
+    public function createDevice($token)
+    {
+        return new Device($token);
     }
 
     /**
